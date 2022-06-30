a1cd1c2d9ed5a050b0c5069798f71e11ed149a33
ThomasLin <thomaslin920@gmail.com>
2022-06-29 23:41:30 -0300

    updated cypress firestore functions

--- cypress/integration/3-my-test/farmForm.spec.js
+++ cypress/integration/3-my-test/farmForm.spec.js
@@ -1,7 +1,25 @@
 describe("After login sccess", () => {
   it("Login Success", () => {
+    const TEST_UID = Cypress.env("TEST_UID");
+    cy.log("uid", TEST_UID);
+
     cy.visit("/");
     cy.login();
+    cy.callFirestore("get", "farms").then((r) => {
+      cy.log("get returned: ", r);
+      if (r.length === 7) {
+        cy.log("arrayNumbers", r.length);
+        cy.log("arrayNumbers", "correct");
+      }
+
+      r.map((item) => {
+        if ((item.uid = TEST_UID)) {
+          cy.log("correct");
+        }
+      });
+    });
+  });
+  it("Create Farm", () => {
     cy.get("[data-testid=click_createFarm]").click();
     cy.get("[data-testid=displayName]").type("thomasFarm");
     cy.get("[data-testid=farmName]").type("myFarm_thomas");
@@ -11,8 +29,20 @@ describe("After login sccess", () => {
     cy.get("[data-testid=click_signout]").should("be.visible");
     cy.get("[data-testid=click_signout]").click();
   });
-  afterEach(() => {
-    cy.logout();
-    cy.clearCookies();
+  it("added Farm", () => {
+    const TEST_UID = Cypress.env("TEST_UID");
+    cy.log("uid", TEST_UID);
+    cy.callFirestore("get", "farms").then((r) => {
+      cy.log("arrayNumbers", r.length);
+      if (r.length === 8) {
+        cy.log("arrayNumbers", "correct");
+      }
+
+      r.map((item) => {
+        if ((item.uid = TEST_UID)) {
+          cy.log("correct");
+        }
+      });
+    });
   });
 });
